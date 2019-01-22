480a0bb945593e847e55598048aef038d454bc9b
xLsDg <xlsdg@qq.com>
2019-01-22 11:16:55 +0800

    chore(mod): upgrade packages

--- config/config.js
+++ config/config.js
@@ -1,37 +1,44 @@
 import LodashWebpackPlugin from 'lodash-webpack-plugin';
 
 const chainWebpack = config => {
-  config.plugin('lodash').use(LodashWebpackPlugin, [{
-    collections: true,
-    paths: true,
-  }]);
+  config.plugin('lodash').use(LodashWebpackPlugin, [
+    {
+      collections: true,
+      paths: true,
+    },
+  ]);
 };
 
 const plugins = [
   // ref: https://umijs.org/plugin/umi-plugin-react.html
-  ['umi-plugin-react', {
-    antd: true,
-    dll: false,
-    dva: false,
-    dynamicImport: {
-      loadingComponent: './components/PageLoading/index.jsx',
-    },
-    fastClick: true,
-    hardSource: false,
-    hd: true,
-    locale: {
+  [
+    'umi-plugin-react',
+    {
       antd: true,
-      baseNavigator: true,
-      default: 'zh-CN',
+      dll: false,
+      dva: {
+        hmr: true,
+      },
+      dynamicImport: {
+        loadingComponent: './components/PageLoading/index.jsx',
+      },
+      fastClick: true,
+      hardSource: false,
+      hd: true,
+      locale: {
+        antd: true,
+        baseNavigator: true,
+        default: 'zh-CN',
+      },
+      routes: {
+        exclude: [],
+      },
+      title: {
+        defaultTitle: 'ANTD MOBILE',
+        useLocale: true,
+      },
     },
-    routes: {
-      exclude: []
-    },
-    title: {
-      defaultTitle: 'ANTD MOBILE',
-      useLocale: true,
-    },
-  }],
+  ],
 ];
 
 // ref: https://umijs.org/config/
