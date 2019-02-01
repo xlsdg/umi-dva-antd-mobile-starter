70d5eaff9d32aed4f44ad7d516f17be4a0059cb6
xLsDg <xlsdg@qq.com>
2019-02-01 08:40:39 +0800

    chore(mod): upgrade packages

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// import { message } from 'antd';
+// import { formatMessage } from 'umi-plugin-react/locale';
+
+export const dva = {
+  config: {
+    onError(error, dispatch) {
+      error.preventDefault();
+      // message.error(formatMessage({ id: 'page.404.request' }));
+    },
+  },
+  // plugins: [],
+};
