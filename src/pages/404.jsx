1d65ea2d9ec19d0d4c3a8b0e9d9ba29dae812b30
xLsDg <xlsdg@qq.com>
2019-04-04 09:34:44 +0800

    chore(pkg): upgrade deps

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,19 @@
 /**
  * title: page.404.title
  */
-
 import React from 'react';
-import { formatMessage } from 'umi-plugin-react/locale';
-import ReactDocumentTitle from 'react-document-title';
+// import { formatMessage } from 'umi-plugin-react/locale';
 
 import styles from './404.less';
 
-export default () => (
-  <ReactDocumentTitle title={formatMessage({ id: 'page.404.title' })}>
-    <div className={styles.main}>404</div>
-  </ReactDocumentTitle>
-);
+NotFound.propTypes = {};
+
+NotFound.defaultProps = {};
+
+function NotFound(props) {
+  return (
+    <div className={styles.container}>404</div>
+  );
+}
+
+export default NotFound;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,19 @@
 /**
  * title: page.index.title
  */
-
 import React from 'react';
-import { formatMessage } from 'umi-plugin-react/locale';
-import ReactDocumentTitle from 'react-document-title';
+// import { formatMessage } from 'umi-plugin-react/locale';
 
 import styles from './index.less';
 
-export default () => (
-  <ReactDocumentTitle title={formatMessage({ id: 'page.index.title' })}>
-    <div className={styles.main}>Index</div>
-  </ReactDocumentTitle>
-);
+Index.propTypes = {};
+
+Index.defaultProps = {};
+
+function Index(props) {
+  return (
+    <div className={styles.container}>Index</div>
+  );
+}
+
+export default Index;
