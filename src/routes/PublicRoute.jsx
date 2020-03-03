feef9abf43280e56e28eecede1c4ff5deac0ad06
xLsDg <xlsdg@qq.com>
2020-03-03 11:43:14 +0800

    chore(pkg): upgrade deps

--- src/routes/PublicRoute.jsx
+++
@@ -1,9 +0,0 @@
-// import _ from 'lodash';
-import Redirect from 'umi/redirect';
-
-export default props => {
-  const { children } = props;
-
-  const isAuth = true;
-  return isAuth ? <Redirect to="/" /> : children;
-};
