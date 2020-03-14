80fb533f9cd72ff9caaf495100695166372f036b
xLsDg <xlsdg@qq.com>
2020-03-14 09:34:13 +0800

    chore(pkg): upgrade deps

--- src/global.js
+++ src/global.js
@@ -1,6 +1,4 @@
-import _ from 'lodash';
-
-if (process.env.NODE_ENV === 'development' || _.includes(['local'], process.env.UMI_ENV)) {
+if (process.env.NODE_ENV === 'development' || process.env.UMI_ENV === 'local') {
   if (!window.VConsole) {
     const VConsole = require('vconsole');
     window.VConsole = new VConsole();
